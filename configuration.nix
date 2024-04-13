0c32f814ac63a0f3bb63e00f82d7dd5f0ff2b0c9
kashifulhaque <haque.kashiful7@gmail.com>
2024-04-13 15:44:55 +0530

    Added flakes

--- configuration.nix
+++ configuration.nix
@@ -31,6 +31,9 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # Enable experimental features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Enable networking
   networking.networkmanager.enable = true;
 
